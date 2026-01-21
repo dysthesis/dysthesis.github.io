54f09ae8d83cffaafbd239619bc2514f2745f30f
Dysthesis <antheoraviel@protonmail.com>
2026-01-21 22:53:14 +1100

    Initial commit

---
+++ contents/hello-world.md
@@ -0,0 +1,10 @@
+---
+title: "Hello, world!"
+subtitle: "A first post."
+ctime: 2026-01-21
+mtime: 2026-01-21
+tags:
+  - hello
+---
+
+This is a first post to mark the start of my new site.
---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Personal site";
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {
+        pkgs,
+        inputs',
+        ...
+      }: {
+        devShells.default = pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            # Nix
+            statix
+            deadnix
+            nixd
+            alejandra
+          ];
+        };
+        packages.default = pkgs.stdenvNoCC.mkDerivation {
+          name = "blog";
+          version = "0.1.0";
+          src = ./.;
+          nativeBuildInputs = [inputs'.ssg.packages.default];
+          buildPhase = ''
+            runHook preBuild
+            ssg
+            runHook postBuild
+          '';
+          installPhase = ''
+            runHook preInstall
+            mkdir -p $out
+            cp -r public/* $out/
+            runHook postInstall
+          '';
+        };
+      };
+    };
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    ssg = {
+      url = "github:dysthesis/ssg";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+}
